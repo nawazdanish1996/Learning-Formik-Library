b13264e5d336f3b8c392157c8d4912295ae64ee2
Nawaz Danish <contact.to.nd@gmail.com>
2023-02-20 22:12:34 +0530

    youtube form

--- src/components/YouTubeForm.jsx
+++ src/components/YouTubeForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "../css/YouTubeForm.scss";
 import { useFormik } from 'formik';
 
@@ -12,27 +12,32 @@ const YouTubeForm = () => {
         }
     })
 
-    console.log(formik);
+    console.log(formik.values);
+
+    const submitHandle = () =>{
+        localStorage.setItem("data", formik.values.name);
+    }
+    
+    useEffect(()=>{
+        let d = new Date();
+        let hr = d.getHours();
+        let min = d.getMinutes();
+        let sec = d.getSeconds();
+        console.log(hr < 10 ? "0"+hr : hr);
+        console.log(min < 10 ? "0"+min : min);
+        console.log(sec < 10 ? "0"+sec : sec);
+    })
 
   return (
     <div className='youtubeForm'>
         <form>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" placeholder='Name' />
-            </div>
-
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" placeholder='Email' />
-            </div>
-
-            <div>
-                <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" placeholder='Channel' />
-            </div>
-
-            <button type="submit">Submit</button>
+            <input onChange={formik.handleChange} value={formik.values.name} type="text" id="name" placeholder='Name' />
+            <br />
+            <input onChange={formik.handleChange} value={formik.values.email} type="email" id="email" placeholder='Email' />
+            <br />
+            <input onChange={formik.handleChange} value={formik.values.channel} type="text" id="channel" placeholder='Channel' />
+            <br />
+            <button onClick={submitHandle} type="submit">Submit</button>
         </form>
     </div>
   )
